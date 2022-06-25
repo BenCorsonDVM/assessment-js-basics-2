94f5a5dab5a9936a9a3f47867f799955078b04d8
Ben Corson <corsonbenjamin@gmail.com>
2022-06-25 18:46:09 -0500

    orders.js complete

--- orders.js
+++ orders.js
@@ -29,6 +29,18 @@
 */
 
 //CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued'
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`)
+    }
+}
 
 /*
     Create a new instance of your class.
@@ -42,6 +54,7 @@
 */
 
 //CODE HERE
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM', 575)
 
 /*
     Call the `updateStatus` method on
@@ -50,3 +63,5 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking')
